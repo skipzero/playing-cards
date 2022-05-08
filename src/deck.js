7f6fc1d90f2e7528f03bfe79db3e611d9cef6676
skip mcbride <skip@angerbunny.com>
2022-05-08 11:08:04 -0700

    fixed silly git issues

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,11 @@
-const cardGame: string = () => {
-  const cardVal: string = ['a','2','3','4','5','6','7','8','9','10','j','q','k'];
-  const cardSuit: string = ['♣︎','♦︎', '♥︎', '♠︎'];
+interface CardGame<Type> {
+  cardVal: Array<string>,
+  cardSuit: Array<string>,
+}
+
+const cardGame: CardGame = () => {
+  const cardVal:  = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
+  const cardSuit: string = ['♣︎', '♦︎', '♥︎', '♠︎'];
   const newDeck: string = cardVal.map((card: string) => {
     return cardSuit.forEach((suit: string) => {
       return card + suit;
