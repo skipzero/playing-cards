09ceb843d8d838ecd8f2fd2ee5359376618ce1e6
skip mcbride <skip@angerbunny.com>
2022-05-09 08:51:48 -0700

    added create react app to boottrap all the react goodness

--- src/deck.js
+++ src/deck.js
@@ -1,19 +1,28 @@
-interface CardGame<Type> {
-  cardVal: Array<string>,
-  cardSuit: Array<string>,
-}
+const cardGame = () => {
 
-const cardGame: CardGame = () => {
-  const cardVal:  = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
-  const cardSuit: string = ['♣︎', '♦︎', '♥︎', '♠︎'];
-  const newDeck: string = cardVal.map((card: string) => {
-    return cardSuit.forEach((suit: string) => {
-      return card + suit;
+  const cardVal = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
+  const cardSuit = ['♣︎', '♦︎', '♥︎', '♠︎'];
+  let newDeck = cardSuit.map((suit) => {
+    return cardVal.map((card) => {
+      return {
+        card,
+        suit
+      }
     })
   })
-  console.log('+++===+++', newDeck)
-  const emptyDeck = newDeck;
-  return emptyDeck;
+
+  const shuffledDeck = (newDeck) => {
+    newDeck = newDeck.flat();
+
+    for (let i = newDeck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let temp = newDeck[i];
+      newDeck[i] = newDeck[j];
+      newDeck[j] = temp;
+    }
+    return newDeck;
+  }
+  console.log('+++===+++', shuffledDeck(newDeck));
 }
 
 cardGame()
